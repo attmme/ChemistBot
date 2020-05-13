4ac79bf9453206474bcf9b0ffa5aeebed1fe812b
lockpicker <vigomga1993@gmail.com>
2020-05-13 23:47:05 +0200

    Movidos bloques de código a la clase robot y comandos

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -4,35 +4,13 @@
 #include "comandos.hpp"
 #include "robot.hpp"
 
-Robot robotet;
-int contador = 0; // temporal
-
 void setup()
 {
 	comando_init();
-	robotet.init();
-
-	Serial.println("Debug");
-	robotet.rotar(true);
+	Serial.println("Debug: Inici!");
 }
 
 void loop()
 {
-	if (robotet.davant_cartutxo())
-	{
-		robotet.rotar(false);
-
-		if (robotet.llegir_cartutxo() == 5) // cartutxo que vol l'usuari
-		{
-			Serial.println("Cartutxo trovat, dosificar");
-			robotet.dosificar_pastilla();
-		}
-		else
-		{
-			Serial.println("Hem passat per un cartutxo, no era el bo");
-			robotet.rotar(true);
-		}
-	}
-
 	pooling_comando(); // comandos.cpp
 }
