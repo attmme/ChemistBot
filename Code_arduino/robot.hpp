21e54f3c85e5df5785591c74c3705e29eeee9d92
lockpicker <vigomga1993@gmail.com>
2020-05-13 23:50:00 +0200

    mejoras de código en algunas funciones como bloqueo y desbloqueo

--- Code_arduino/robot.hpp
+++ Code_arduino/robot.hpp
@@ -4,26 +4,41 @@
 // Macros
 #define N_MESURES 15   // + mesures == + estable la mesura
 #define PES_CONEGUT 50 // pes per a calibrar
+
+#define TEMPS_AJUSTE_POSICIO_CARRO 75 // temps per a ajustar millor la posició
+
 #define MOTOR_ROTOR_PWM_MAXIM 50
+
+#define MIN_SERVO_LECTOR 0
+#define MAX_SERVO_LECTOR 45
+
+#define MIN_SERVO_PASTILLA 10 // dispensador pastilla
+#define MAX_SERVO_PASTILLA 174
+
+#define TEMPS_ESPERA_LECTURA 300
+
+// Bloqueig motor
+#define STOP 0
+#define BLOQ 2
+#define DESBLOQ 4
 
 class Robot
 {
 public:
-	void init();
-
 	// Balança
 	static int step_calib; // calibrar té uns passos, amb això ho controlem
 	static int si;		   // cops que l'usuari diu que si en la calibració
 	static int no;		   // cops que l'usuari diu que no en la calibració
 	static bool calibrant; // quan acaba la calibració, els comandos tornen a funcionar normal
-	static int variable_temporal;
+
+	void init();
 
 	void tara();
 	void calibrar();
 	float llegir_pes();
 
 	// Motor rotor
-	void rotar(bool si_no);
+	void girar(bool si_no);
 
 	// Sensor rotor
 	bool davant_cartutxo();
@@ -33,6 +48,9 @@ public:
 
 	// Sensors cartutxo
 	int llegir_cartutxo();
+
+	// Bloqueig rotor
+	void tambor(char blck_desbl);
 
 private:
 	// Balança
@@ -46,6 +64,11 @@ private:
 	// Motor dispensador cartutxo
 
 	// Sensors cartutxo
+
+	// Bloqueig rotor
+	void bloquejar_rotor();
+	void desbloquejar_rotor();
+	void bloq_desbloq(char rotacio);
 };
 
 #endif
