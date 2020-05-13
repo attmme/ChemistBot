2262c5334ba20f1f414d548a2883c91ea518848c
lockpicker <vigomga1993@gmail.com>
2020-05-13 23:50:25 +0200

    añadidas nuevas strings para los nuevos comandos

--- Code_arduino/strings_comandos.h
+++ Code_arduino/strings_comandos.h
@@ -1,16 +1,21 @@
 #ifndef _strings_comandos_h_
 #define _strings_comandos_h_
 
-// Defines
+// Defines. El número ha de concidir amb el string comando[]
 #define ERROR -1
-#define COMANDO_SI 0		// El número ha de concidir amb el string comando[]
+#define COMANDO_SI 0
 #define COMANDO_NO 1
 #define BALANCA_CALIB 2
 #define BALANCA_TARA 3
 #define BALANCA_GET_PES 4
-#define COMANDO_TEMPORAL 5
+#define	ROBOT_DOSIFICAR 5
 
 #define NUM_MAX_COMANDOS 6
+
+// Tipus de missatges per la terminal:
+//"Debug: "
+//"Info: "
+//"Error: "
 
 // Strings comandos
 String comando[NUM_MAX_COMANDOS] =
@@ -20,7 +25,7 @@ String comando[NUM_MAX_COMANDOS] =
 		"calibrar", // i = 2
 		"tara",		// i = 3
 		"pes",		// i = 4
-		"cart",		// i = 5
+		"dosificar", // i = 5
 };
 
 // Strings d'errors
