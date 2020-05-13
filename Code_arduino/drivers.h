86d7ab5a570b6da2ef50420fdc2a196a86152e52
lockpicker <vigomga1993@gmail.com>
2020-05-13 23:48:59 +0200

    añadidos nuevos pines, como el motor de bloqueo del rotor

--- Code_arduino/drivers.h
+++ Code_arduino/drivers.h
@@ -1,18 +1,31 @@
 #ifndef _drivers_h_
 #define _drivers_h_
 
-// Bascula
-const int LOADCELL_DOUT_PIN = 2; // DATA
-const int LOADCELL_SCK_PIN = 3;  // CLK
+// Bascula  P27
+#define BASCULA_DATA 2 // DATA
+#define BASCULA_SCK 3  // CLK
 
-// Motor rotor
+// tmp: arduino mega2560, pins pwm => 2~13 i 44~46
+
+// Motor rotor P33
 #define MOTOR_ROTOR 11
 
-// Sensor rotor
+// Sensor rotor P29
 #define PIN_SENSOR_ROTOR 5
 
-// Motor dispensador cartutxo
+// Servos dispensador cartutxo / lector P30, P32
+#define PIN_DISPENSADOR 6
+#define PIN_LECTOR 8
 
-// Sensors cartutxo
+// Sensors cartutxo  ( P7, P8)
+#define SENSOR_0 48 // P7
+#define SENSOR_1 49 // P7
+#define SENSOR_2 47 // P8
+
+// Bloqueig/desbloqueig
+#define SENSOR_INICI 53 // P1
+#define SENSOR_FINAL 52
+#define CONTROL_A 51 // P2
+#define CONTROL_B 50
 
 #endif
